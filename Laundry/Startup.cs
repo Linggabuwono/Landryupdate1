c015d2860c9e876db8878404e9d914d8c5058b14
Lingga Buwono <lingga.buwono.ft18@mail.umy.ac.id>
2021-01-05 22:20:46 +0700

    Berpusing-pusing

--- Laundry/Startup.cs
+++ Laundry/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -25,16 +26,25 @@ namespace Laundry
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
+            /*services.AddControllersWithViews();*/
+
+            /*services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
+            });*/
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.ViewLocationFormats.Clear();
+                options.ViewLocationFormats.Add("/NewViews/{1}/{0}" + RazorViewEngine.ViewExtension);
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                /*options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;*/
             });
-
             services.AddDbContext<Models.LaundryContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("Default")));
-
+            
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -54,6 +64,7 @@ namespace Laundry
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+            
 
             app.Use(async (context, next) =>
             {
@@ -94,8 +105,10 @@ namespace Laundry
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=ReadOnly}/{action=Index}/{id?}");
             });
+
+
         }
     }
 }
